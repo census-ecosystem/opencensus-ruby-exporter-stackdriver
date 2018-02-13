577c215f5bf59005eeaf61742bd131468c02991b
Daniel Azuma <dazuma@gmail.com>
2018-02-13 15:34:46 -0800

    Add converter for stackdriver trace protos (#1)

---
+++ lib/opencensus/trace/exporters/stackdriver/converter.rb
@@ -0,0 +1,356 @@
+# Copyright 2018 OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "google/devtools/cloudtrace/v2/trace_pb"
+require "google/protobuf/well_known_types"
+
+module OpenCensus
+  module Trace
+    module Exporters
+      class Stackdriver
+        ##
+        # An object that converts OpenCensus span data objects to Stackdriver
+        # Trace V2 protos.
+        #
+        # You should use one converter instance to convert the spans for a
+        # single export request, because the converter will keep track of and
+        # omit duplicate stack traces. Use a new converter instance for the
+        # next request.
+        #
+        # @private
+        #
+        class Converter
+          ##
+          # @private
+          # Alias for the V2 Cloudtrace protos namespace
+          #
+          TraceProtos = Google::Devtools::Cloudtrace::V2
+
+          ##
+          # Create a converter
+          #
+          # @param [String] project_id Google project ID
+          #
+          def initialize project_id
+            @project_id = project_id
+            @stack_trace_hash_ids = {}
+          end
+
+          # rubocop:disable Metrics/AbcSize
+
+          ##
+          # Convert a span object.
+          #
+          # @param [OpenCensus::Trace::Span] obj OpenCensus span object
+          # @return [Google::Devtools::Cloudtrace::V2::Span] The generated
+          #     proto
+          #
+          def convert_span obj
+            TraceProtos::Span.new \
+              name: make_resource_name(@project_id, obj.trace_id, obj.span_id),
+              span_id: obj.span_id,
+              parent_span_id: obj.parent_span_id,
+              display_name: convert_truncatable_string(obj.name),
+              start_time: convert_time(obj.start_time),
+              end_time: convert_time(obj.end_time),
+              attributes:
+                convert_attributes(obj.attributes,
+                                   obj.dropped_attributes_count),
+              stack_trace:
+                convert_stack_trace(obj.stack_trace, obj.dropped_frames_count,
+                                    obj.stack_trace_hash_id),
+              time_events:
+                convert_time_events(obj.time_events,
+                                    obj.dropped_annotations_count,
+                                    obj.dropped_message_events_count),
+              links: convert_links(obj.links, obj.dropped_links_count),
+              status: convert_optional_status(obj.status),
+              same_process_as_parent_span:
+                convert_optional_bool(obj.same_process_as_parent_span),
+              child_span_count: convert_optional_int32(obj.child_span_count)
+          end
+
+          # rubocop:enable Metrics/AbcSize
+
+          ##
+          # Make a span resource name.
+          #
+          # @param [String] project_id The project ID
+          # @param [String] trace_id The project ID
+          # @param [String] span_id The project ID
+          # @return [String] The resource na,e
+          #
+          def make_resource_name project_id, trace_id, span_id
+            "projects/#{project_id}/traces/#{trace_id}/spans/#{span_id}"
+          end
+
+          ##
+          # Create a truncatable string proto.
+          #
+          # @param [String] str The string
+          # @param [Integer] truncated_byte_count The number of bytes omitted.
+          #     Defaults to 0.
+          # @return [Google::Devtools::Cloudtrace::V2::TruncatableString] The
+          #     generated proto
+          #
+          def make_truncatable_string str, truncated_byte_count = 0
+            TraceProtos::TruncatableString.new \
+              value: str,
+              truncated_byte_count: truncated_byte_count
+          end
+
+          ##
+          # Convert a truncatable string object.
+          #
+          # @param [OpenCensus::Trace::TruncatableString] obj OpenCensus
+          #     truncatable string object
+          # @return [Google::Devtools::Cloudtrace::V2::TruncatableString] The
+          #     generated proto
+          #
+          def convert_truncatable_string obj
+            make_truncatable_string obj.value, obj.truncated_byte_count
+          end
+
+          ##
+          # Convert a time object.
+          #
+          # @param [Time] time Ruby Time object
+          # @return [Google::Protobuf::Timestamp] The generated proto
+          #
+          def convert_time time
+            proto = Google::Protobuf::Timestamp.new
+            proto.from_time(time)
+            proto
+          end
+
+          ##
+          # Convert a value that can be used for an attribute.
+          #
+          # @param [OpenCensus::Trace::TruncatableString, Integer, boolean]
+          #     obj Object to convert
+          # @return [Google::Devtools::Cloudtrace::V2::AttributeValue] The
+          #     generated proto
+          #
+          def convert_attribute_value obj
+            case obj
+            when OpenCensus::Trace::TruncatableString
+              TraceProtos::AttributeValue.new \
+                string_value: convert_truncatable_string(obj)
+            when Integer
+              TraceProtos::AttributeValue.new int_value: obj
+            when true, false
+              TraceProtos::AttributeValue.new bool_value: obj
+            end
+          end
+
+          ##
+          # Convert an attributes hash
+          #
+          # @param [Hash] attributes The map of attribute values to convert
+          # @param [Integer] dropped_attributes_count Number of dropped
+          # @return [Google::Devtools::Cloudtrace::V2::Attributes] The
+          #     generated proto
+          #
+          def convert_attributes attributes, dropped_attributes_count
+            attribute_map = {}
+            attributes.each do |k, v|
+              attribute_map[k] = convert_attribute_value v
+            end
+            TraceProtos::Span::Attributes.new \
+              attribute_map: attribute_map,
+              dropped_attributes_count: dropped_attributes_count
+          end
+
+          ##
+          # Convert a single stack frame as a Thread::Backtrace::Location
+          #
+          # @param [Thread::Backtrace::Location] frame The backtrace element to
+          #     convert
+          # @return [Google::Devtools::Cloudtrace::V2::StackTrace::StackFrame]
+          #     The generated proto
+          #
+          def convert_stack_frame frame
+            TraceProtos::StackTrace::StackFrame.new \
+              function_name: make_truncatable_string(frame.label),
+              file_name: make_truncatable_string(frame.path),
+              line_number: frame.lineno
+          end
+
+          ##
+          # Convert a full backtrace.
+          #
+          # @param [Array<Thread::Backtrace::Location>] backtrace The backtrace
+          #     element array to convert
+          # @param [Integer] dropped_frames_count Frames that were dropped
+          # @param [Integer] stack_trace_hash_id Hash of the data
+          # @return [Google::Devtools::Cloudtrace::V2::StackTrace] The
+          #     generated proto
+          #
+          def convert_stack_trace backtrace, dropped_frames_count,
+                                  stack_trace_hash_id
+            if @stack_trace_hash_ids[stack_trace_hash_id]
+              return TraceProtos::StackTrace.new \
+                stack_trace_hash_id: stack_trace_hash_id
+            end
+            @stack_trace_hash_ids[stack_trace_hash_id] = true
+            frame_protos = backtrace.map { |frame| convert_stack_frame(frame) }
+            frames_proto = TraceProtos::StackTrace::StackFrames.new \
+              frame: frame_protos,
+              dropped_frames_count: dropped_frames_count
+            TraceProtos::StackTrace.new \
+              stack_frames: frames_proto,
+              stack_trace_hash_id: stack_trace_hash_id
+          end
+
+          ##
+          # Convert an annotation object
+          #
+          # @param [OpenCensus::Trace::Annotation] annotation The annotation
+          #     object to convert
+          # @return
+          #     [Google::Devtools::Cloudtrace::V2::Span::TimeEvent::Annotation]
+          #     The generated proto
+          #
+          def convert_annotation annotation
+            annotation_proto = TraceProtos::Span::TimeEvent::Annotation.new \
+              description: convert_truncatable_string(annotation.description),
+              attributes:
+                convert_attributes(annotation.attributes,
+                                   annotation.dropped_attributes_count)
+            TraceProtos::Span::TimeEvent.new \
+              time: convert_time(annotation.time),
+              annotation: annotation_proto
+          end
+
+          ##
+          # Convert a message event object
+          #
+          # @param [OpenCensus::Trace::MessageEvent] message_event The message
+          #     event object to convert
+          # @return
+          #    [Google::Devtools::Cloudtrace::V2::Span::TimeEvent::MessageEvent]
+          #    The generated proto
+          #
+          def convert_message_event message_event
+            message_event_proto =
+              TraceProtos::Span::TimeEvent::MessageEvent.new \
+                type: message_event.type,
+                id: message_event.id,
+                uncompressed_size_bytes: message_event.uncompressed_size,
+                compressed_size_bytes: message_event.compressed_size
+            Google::Devtools::Cloudtrace::V2::Span::TimeEvent.new \
+              time: convert_time(message_event.time),
+              message_event: message_event_proto
+          end
+
+          ##
+          # Convert a list of time event objects
+          #
+          # @param [Array<OpenCensus::Trace::TimeEvent>] time_events The time
+          #     event objects to convert
+          # @param [Integer] dropped_annotations_count Number of dropped
+          #     annotations
+          # @param [Integer] dropped_message_events_count Number of dropped
+          #     message events
+          # @return [Google::Devtools::Cloudtrace::V2::Span::TimeEvents] The
+          #     generated proto
+          #
+          def convert_time_events time_events, dropped_annotations_count,
+                                  dropped_message_events_count
+            time_event_protos = time_events.map do |time_event|
+              case time_event
+              when OpenCensus::Trace::Annotation
+                convert_annotation time_event
+              when OpenCensus::Trace::MessageEvent
+                convert_message_event time_event
+              else
+                nil
+              end
+            end.compact
+            TraceProtos::Span::TimeEvents.new \
+              time_event: time_event_protos,
+              dropped_annotations_count: dropped_annotations_count,
+              dropped_message_events_count: dropped_message_events_count
+          end
+
+          ##
+          # Convert a link object
+          #
+          # @param [OpenCensus::Trace::Link] link The link object to convert
+          # @return [Google::Devtools::Cloudtrace::V2::Span::Link] The
+          #     generated proto
+          #
+          def convert_link link
+            TraceProtos::Span::Link.new \
+              trace_id: link.trace_id,
+              span_id: link.span_id,
+              type: link.type,
+              attributes:
+                convert_attributes(link.attributes,
+                                   link.dropped_attributes_count)
+          end
+
+          ##
+          # Convert a list of link objects
+          #
+          # @param [Array<OpenCensus::Trace::Link>] links The link objects to
+          #     convert
+          # @param [Integer] dropped_links_count Number of dropped links
+          # @return [Google::Devtools::Cloudtrace::V2::Span::Links] The
+          #     generated proto
+          #
+          def convert_links links, dropped_links_count
+            TraceProtos::Span::Links.new \
+              link: links.map { |link| convert_link link },
+              dropped_links_count: dropped_links_count
+          end
+
+          ##
+          # Convert a nullable status object
+          #
+          # @param [OpenCensus::Trace::Status, nil] status The status object to
+          #     convert, or nil if absent
+          # @return [Google::Rpc::Status, nil] The generated proto, or nil
+          #
+          def convert_optional_status status
+            return nil if status.nil?
+            Google::Rpc::Status.new code: status.code, message: status.message
+          end
+
+          ##
+          # Convert a nullable boolean object
+          #
+          # @param [boolean, nil] value The value to convert, or nil if absent
+          # @return [Google::Protobuf::BoolValue, nil] Generated proto, or nil
+          #
+          def convert_optional_bool value
+            return nil if value.nil?
+            Google::Protobuf::BoolValue.new value: value
+          end
+
+          ##
+          # Convert a nullable int32 object
+          #
+          # @param [Integer, nil] value The value to convert, or nil if absent
+          # @return [Google::Protobuf::Int32Value, nil] Generated proto, or nil
+          #
+          def convert_optional_int32 value
+            return nil if value.nil?
+            Google::Protobuf::Int32Value.new value: value
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/converter_test.rb
@@ -0,0 +1,274 @@
+# Copyright 2018 OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "test_helper"
+
+describe OpenCensus::Trace::Exporters::Stackdriver::Converter do
+  let(:project_id) { "my-project" }
+  let(:converter) {
+    OpenCensus::Trace::Exporters::Stackdriver::Converter.new project_id
+  }
+  let(:simple_string) { "hello" }
+  let(:string_truncated_bytes) { 6 }
+  let(:truncatable_string) {
+    OpenCensus::Trace::TruncatableString.new \
+      simple_string, truncated_byte_count: string_truncated_bytes
+  }
+  let(:trace_id) { "0123456789abcdef0123456789abcdef" }
+  let(:trace_id2) { "fedcba9876543210fedcba9876543210" }
+  let(:span_id) { "0123456789abcdef" }
+  let(:span_id2) { "fedcba9876543210" }
+  let(:annotation_desc) { "This is an annotation" }
+  let(:annotation) {
+    OpenCensus::Trace::Annotation.new \
+      OpenCensus::Trace::TruncatableString.new(annotation_desc),
+      attributes: {"foo" => truncatable_string},
+      dropped_attributes_count: 1,
+      time: Time.at(1001)
+  }
+  let(:message_event) {
+    OpenCensus::Trace::MessageEvent.new \
+      OpenCensus::Trace::MessageEvent::SENT, 12345, 100, time: Time.at(1002)
+  }
+  let(:link1) {
+    OpenCensus::Trace::Link.new trace_id, span_id,
+      type: OpenCensus::Trace::Link::CHILD_LINKED_SPAN,
+      attributes: {"foo" => truncatable_string},
+      dropped_attributes_count: 1
+  }
+  let(:link2) {
+    OpenCensus::Trace::Link.new trace_id2, span_id2,
+      type: OpenCensus::Trace::Link::PARENT_LINKED_SPAN,
+      attributes: {"foo" => 123},
+      dropped_attributes_count: 2
+  }
+  let(:loc1) {
+    OpenStruct.new label: "foo", path: "/path/to/file.rb", lineno: 100
+  }
+  let(:loc2) {
+    OpenStruct.new label: "bar", path: "/path/to/another/file.rb", lineno: 200
+  }
+  let(:status) {
+    OpenCensus::Trace::Status.new 404, "Not found"
+  }
+
+  describe "#convert_truncatable_string" do
+    it "converts a fully populated object" do
+      proto = converter.convert_truncatable_string truncatable_string
+      proto.value.must_equal simple_string
+      proto.truncated_byte_count.must_equal string_truncated_bytes
+    end
+  end
+
+  describe "#convert_attribute_value" do
+    it "converts a string" do
+      proto = converter.convert_attribute_value truncatable_string
+      proto.string_value.value.must_equal simple_string
+      proto.string_value.truncated_byte_count.must_equal string_truncated_bytes
+      proto.value.must_equal :string_value
+    end
+
+    it "converts an integer" do
+      proto = converter.convert_attribute_value(-1000)
+      proto.int_value.must_equal(-1000)
+      proto.value.must_equal :int_value
+    end
+
+    it "converts a boolean" do
+      proto = converter.convert_attribute_value false
+      proto.bool_value.must_equal false
+      proto.value.must_equal :bool_value
+    end
+  end
+
+  describe "#convert_attributes" do
+    it "converts three attributes of different types" do
+      input_attrs = {"str" => truncatable_string, "int" => -1000, "bool" => false}
+      proto = converter.convert_attributes input_attrs, 2
+      proto.attribute_map["str"].string_value.value.must_equal simple_string
+      proto.attribute_map["str"].string_value.truncated_byte_count.must_equal string_truncated_bytes
+      proto.attribute_map["str"].value.must_equal :string_value
+      proto.attribute_map["int"].int_value.must_equal(-1000)
+      proto.attribute_map["int"].value.must_equal :int_value
+      proto.attribute_map["bool"].bool_value.must_equal false
+      proto.attribute_map["bool"].value.must_equal :bool_value
+      proto.dropped_attributes_count.must_equal 2
+    end
+  end
+
+  describe "#convert_stack_frame" do
+    it "converts an ordinary location" do
+      proto = converter.convert_stack_frame loc1
+      proto.function_name.value.must_equal "foo"
+      proto.function_name.truncated_byte_count.must_equal 0
+      proto.file_name.value.must_equal "/path/to/file.rb"
+      proto.file_name.truncated_byte_count.must_equal 0
+      proto.line_number.must_equal 100
+    end
+  end
+
+  describe "#convert_stack_trace" do
+    it "converts an ordinary location" do
+      proto = converter.convert_stack_trace [loc1, loc2], 3, 12345
+      proto.stack_frames.frame.length.must_equal 2
+      proto.stack_frames.frame[0].function_name.value.must_equal "foo"
+      proto.stack_frames.frame[1].function_name.value.must_equal "bar"
+      proto.stack_frames.dropped_frames_count.must_equal 3
+      proto.stack_trace_hash_id.must_equal 12345
+    end
+
+    it "omits a duplicate" do
+      proto1 = converter.convert_stack_trace [loc1, loc2], 3, 12345
+      proto1.stack_frames.wont_be_nil
+      proto1.stack_trace_hash_id.must_equal 12345
+      proto2 = converter.convert_stack_trace [loc1, loc2], 3, 12345
+      proto2.stack_frames.must_be_nil
+      proto2.stack_trace_hash_id.must_equal 12345
+    end
+  end
+
+  describe "#convert_annotation" do
+    it "converts an annotation" do
+      proto = converter.convert_annotation annotation
+      proto.annotation.description.value.must_equal annotation_desc
+      proto.annotation.description.truncated_byte_count.must_equal 0
+      proto.annotation.attributes.dropped_attributes_count.must_equal 1
+      proto.annotation.attributes.attribute_map["foo"].string_value.value.must_equal simple_string
+      proto.annotation.attributes.attribute_map["foo"].string_value.truncated_byte_count.must_equal string_truncated_bytes
+      proto.time.seconds.must_equal 1001
+    end
+  end
+
+  describe "#convert_message_event" do
+    it "converts a message event" do
+      proto = converter.convert_message_event message_event
+      proto.message_event.type.must_equal :SENT
+      proto.message_event.id.must_equal 12345
+      proto.message_event.uncompressed_size_bytes.must_equal 100
+      proto.message_event.compressed_size_bytes.must_equal 0
+      proto.time.seconds.must_equal 1002
+    end
+  end
+
+  describe "#convert_time_events" do
+    it "converts a set of time events" do
+      proto = converter.convert_time_events [message_event, annotation], 4, 5
+      proto.time_event.length.must_equal 2
+      proto.time_event[0].message_event.type.must_equal :SENT
+      proto.time_event[1].annotation.description.value.must_equal annotation_desc
+      proto.dropped_annotations_count.must_equal 4
+      proto.dropped_message_events_count.must_equal 5
+    end
+  end
+
+  describe "#convert_link" do
+    it "converts a link" do
+      proto = converter.convert_link link1
+      proto.trace_id.must_equal trace_id
+      proto.span_id.must_equal span_id
+      proto.type.must_equal :CHILD_LINKED_SPAN
+      proto.attributes.dropped_attributes_count.must_equal 1
+      proto.attributes.attribute_map["foo"].string_value.value.must_equal simple_string
+    end
+  end
+
+  describe "#convert_links" do
+    it "converts a set of links" do
+      proto = converter.convert_links [link1, link2], 3
+      proto.link.length.must_equal 2
+      proto.link[0].type.must_equal :CHILD_LINKED_SPAN
+      proto.link[1].type.must_equal :PARENT_LINKED_SPAN
+      proto.dropped_links_count.must_equal 3
+    end
+  end
+
+  describe "#convert_status" do
+    it "converts a status" do
+      proto = converter.convert_optional_status status
+      proto.code.must_equal 404
+      proto.message.must_equal "Not found"
+    end
+
+    it "converts nil" do
+      proto = converter.convert_optional_status nil
+      proto.must_be_nil
+    end
+  end
+
+  describe "#convert_span" do
+    it "converts a basic span" do
+      span = OpenCensus::Trace::Span.new \
+        trace_id, span_id, truncatable_string, Time.at(1000), Time.at(2000),
+        parent_span_id: span_id2,
+        attributes: {"foo" => 123},
+        dropped_attributes_count: 1,
+        stack_trace: [loc1, loc2],
+        dropped_frames_count: 2,
+        time_events: [annotation, message_event],
+        dropped_annotations_count: 3,
+        dropped_message_events_count: 4,
+        links: [link1, link2],
+        dropped_links_count: 5,
+        status: status,
+        same_process_as_parent_span: true,
+        child_span_count: 6
+      proto = converter.convert_span span
+      proto.name.must_equal "projects/#{project_id}/traces/#{trace_id}/spans/#{span_id}"
+      proto.span_id.must_equal span_id
+      proto.parent_span_id.must_equal span_id2
+      proto.display_name.value.must_equal simple_string
+      proto.start_time.seconds.must_equal 1000
+      proto.end_time.seconds.must_equal 2000
+      proto.attributes.attribute_map["foo"].int_value.must_equal 123
+      proto.attributes.dropped_attributes_count.must_equal 1
+      proto.stack_trace.stack_frames.frame[0].function_name.value.must_equal "foo"
+      proto.stack_trace.stack_frames.dropped_frames_count.must_equal 2
+      proto.time_events.time_event[0].time.seconds.must_equal 1001
+      proto.time_events.time_event[0].annotation.description.value.must_equal annotation_desc
+      proto.time_events.time_event[1].time.seconds.must_equal 1002
+      proto.time_events.time_event[1].message_event.type.must_equal :SENT
+      proto.time_events.dropped_annotations_count.must_equal 3
+      proto.time_events.dropped_message_events_count.must_equal 4
+      proto.links.link[0].type.must_equal :CHILD_LINKED_SPAN
+      proto.links.link[1].type.must_equal :PARENT_LINKED_SPAN
+      proto.links.dropped_links_count.must_equal 5
+      proto.status.code.must_equal 404
+      proto.same_process_as_parent_span.value.must_equal true
+      proto.child_span_count.value.must_equal 6
+    end
+
+    it "caches repeated stack traces" do
+      span = OpenCensus::Trace::Span.new \
+        trace_id, span_id, truncatable_string, Time.at(1000), Time.at(2000),
+        parent_span_id: span_id2,
+        attributes: {"foo" => 123},
+        dropped_attributes_count: 1,
+        stack_trace: [loc1, loc2],
+        dropped_frames_count: 2,
+        time_events: [annotation, message_event],
+        dropped_annotations_count: 3,
+        dropped_message_events_count: 4,
+        links: [link1, link2],
+        dropped_links_count: 5,
+        status: status,
+        same_process_as_parent_span: true,
+        child_span_count: 6
+      proto1 = converter.convert_span span
+      proto2 = converter.convert_span span
+      proto1.stack_trace.stack_frames.wont_be_nil
+      proto2.stack_trace.stack_frames.must_be_nil
+      proto2.stack_trace.stack_trace_hash_id.must_equal proto1.stack_trace.stack_trace_hash_id
+    end
+  end
+end
