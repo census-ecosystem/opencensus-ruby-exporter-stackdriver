90c10b3343c20443ddbf0fedeca2000de1362d21
Daniel Azuma <dazuma@google.com>
2018-03-05 15:11:27 -0500

    Provide dummy credentials to the test so it doesn't try to pull ADC

--- test/exporter_test.rb
+++ test/exporter_test.rb
@@ -48,7 +48,8 @@ describe OpenCensus::Trace::Exporters::Stackdriver do
 
     exporter = OpenCensus::Trace::Exporters::Stackdriver.new \
       project_id: project_id,
-      mock_client: mock_client
+      mock_client: mock_client,
+      credentials: :dummy_credentials
 
     exporter.export [span1, span2]
     exporter.shutdown
